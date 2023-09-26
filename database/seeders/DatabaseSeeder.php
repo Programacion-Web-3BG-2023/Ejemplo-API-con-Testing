836e69b7c8ae1298a84ef7bb9ebd5b830753c7b7
Profesor <ggmartinez.utu@gmail.com>
2023-09-26 14:39:45 -0300

    Agregada config de actions, y archivo de DB

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Persona::factory(10)->create();
+        \App\Models\Persona::factory(100)->create();
         \App\Models\Persona::factory(1)->create([
             "id" => "999",
             "nombre" => "Juan",
---
+++ db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE personas(
+    id int primary key auto_increment
+    nombre varchar(255),
+    apellido varchar(255),
+    created_at timestamp,
+    updated_at timestamp,
+    deleted_at timestap
+)
